25278fe26d57a793d715b721214e2d4b26962b35
Rodrigo Jose Sarmento Oliveira <rodrigojsoliveira@gmail.com>
2019-01-27 10:31:45 -0200

    Conform itemCatalogDatabase_setup.py to PEP8

--- itemCatalogDatabase_setup.py
+++ itemCatalogDatabase_setup.py
@@ -6,16 +6,18 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key = True)
-    username = Column(String(30), nullable = False)
-    email = Column(String(50), nullable = False)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(30), nullable=False)
+    email = Column(String(50), nullable=False)
+
 
 class Category(Base):
     __tablename__ = 'categories'
-    id = Column(Integer, primary_key = True)
-    name = Column(String(30), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30), nullable=False)
 
     @property
     def serialize(self):
@@ -24,14 +26,15 @@ class Category(Base):
             'name': self.name
         }
 
+
 class Item(Base):
     __tablename__ = 'items'
-    id = Column(Integer, primary_key = True)
-    name = Column(String(30), nullable = False)
-    description = Column(String(300), nullable = False)
-    category_id = Column(Integer, ForeignKey('categories.id'), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30), nullable=False)
+    description = Column(String(300), nullable=False)
+    category_id = Column(Integer, ForeignKey('categories.id'), nullable=False)
     category = relationship('Category')
-    user_id = Column(Integer, ForeignKey('users.id'), nullable = False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     user = relationship('User')
 
     @property
